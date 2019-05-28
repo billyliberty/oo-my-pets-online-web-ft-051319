d18d685244a1e97481428f45efdc7d473fae0f70
Joshua Crum <joshua.crum@gmail.com>
2019-05-28 23:34:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,7 +27,24 @@ class Owner
   end
 
   def buy_dog
-    pets[:dog] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+   def walk_dogs
+    @pets[:dogs].each{|dog| dog.mood = "happy"}
+  end
+
+   def play_with_cats
+    @pets[:cats].each{|cat| cat.mood = "happy"}
+  end
+
+   def feed_fish
+    @pets[:fishes].each{|fish| fish.mood = "happy"}
+  end
+
+   def sell_pets
+    @pets.each{|type,names| names.map{|pet| pet.mood = "nervous"}}
+    pets.clear
   end
 
   def list_pets
